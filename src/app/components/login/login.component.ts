58fa0cddfde771bef99f76e4cc6051af7cd7a577
joshuatrefzer <121818554+joshuatrefzer@users.noreply.github.com>
2023-09-21 15:50:41 +0200

    push

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { lastValueFrom } from 'rxjs';
+import { AuthService } from 'src/app/services/auth.service';
+import { environment } from 'src/environments/environment.prod';
 
 @Component({
   selector: 'app-login',
@@ -6,13 +10,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
-  email:any;
-  password:any;
-  
-  
-  
-  
-  login(){
+  username: string = '';
+  password: string = '';
 
+  constructor( private as:AuthService) { }
+
+
+  async login() {
+  
+    try {
+      let resp = await this.as.loginWithUsernameAndPassword(this.username , this.password);
+      console.log(resp);
+      
+    } catch (e) {
+      console.log(e);
+      
+    }
   }
 }
